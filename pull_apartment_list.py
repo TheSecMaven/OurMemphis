1ea53abcbbdfc8d37b1a3a188b732cc57272b2a0
Miclain Keffeler <mkkeffeler@crimson.ua.edu>
2017-06-13 16:54:20 -0500

    Pull more results from all parts of memphis (N,S,E,SE,NE)

--- pull_apartment_list.py
+++ pull_apartment_list.py
@@ -4,14 +4,13 @@ import time
 import os
 
 #Grabbing and parsing the JSON data
-def GoogPlac(lat,lng,radius,types,key,Apartments):
+def GoogPlac(radius,types,key,Apartments):
   #making the url
   AUTH_KEY = key
-  LOCATION = str(lat) + "," + str(lng)
   RADIUS = radius
   TYPES = types
   MyUrl = ('https://maps.googleapis.com/maps/api/place/textsearch/json'
-           '?query=%s&location=%s&radius=%s&key=%s') % (TYPES,LOCATION,RADIUS,AUTH_KEY)
+           '?query=%s&key=%s') % (TYPES,AUTH_KEY)
   #grabbing the JSON result
   response = requests.get(MyUrl)
   jsonRaw = response.json()
@@ -34,40 +33,20 @@ def IterJson(place):
 path = '/home/pi/OurMemphis/apartmentdata'
 os.chdir(path)
 MyKey = 'AIzaSyDlIVa-Ub4V6nRg5uwLO4I8utDpm2VX1kU'
-MyType = 'memphis+apartments'
-Apartments = open("/home/pi/OurMemphis/apartmentdata/Apartments_in_memphis5.json","w")
-all_json = GoogPlac(35.136749,-90.032631,100,MyType,MyKey,Apartments)
-Apartments = open("/home/pi/OurMemphis/apartmentdata/Apartments_in_memphis6.json","w")
-all_json1 = GoogPlac(35.177747,-89.969617,100,MyType,MyKey,Apartments)
-Apartments = open("/home/pi/OurMemphis/apartmentdata/Apartments_in_memphis7.json","w")
-all_json2 = GoogPlac(35.167925,-89.920178,100,MyType,MyKey,Apartments)
-Apartments = open("/home/pi/OurMemphis/apartmentdata/Apartments_in_memphi83.json","w")
-all_json3 = GoogPlac(35.140416,-89.901639,100,MyType,MyKey,Apartments)
-Apartments = open("/home/pi/OurMemphis/apartmentdata/Apartments_in_memphis49.json","w")
-all_json4 = GoogPlac(35.113460,-89.890309,100,MyType,MyKey,Apartments)
-count = 2
-while(all_json['next_page_token'] is not 'null'):
-    Apartments = open("/home/pi/OurMemphis/apartmentdata/Apartments_in_memphis" + str(count) + ".json","w")
-    time.sleep(15)
-    all_json = next_page(MyKey,str(all_json['next_page_token']),Apartments)
-    count += 1
-while(all_json1['next_page_token'] is not 'null'):
-    Apartments = open("/home/pi/OurMemphis/apartmentdata/Apartments_in_memphis" + str(count) + ".json","w")
-    time.sleep(15)
-    all_json1 = next_page(MyKey,str(all_json['next_page_token']),Apartments)
-    count += 1
-while(all_json2['next_page_token'] is not 'null'):
-    Apartments = open("/home/pi/OurMemphis/apartmentdata/Apartments_in_memphis" + str(count) + ".json","w")
-    time.sleep(15)
-    all_json2 = next_page(MyKey,str(all_json['next_page_token']),Apartments)
-    count += 1
-while(all_json3['next_page_token'] is not 'null'):
-    Apartments = open("/home/pi/OurMemphis/apartmentdata/Apartments_in_memphis" + str(count) + ".json","w")
-    time.sleep(15)
-    all_json3 = next_page(MyKey,str(all_json['next_page_token']),Apartments)
-    count += 1
-while(all_json4['next_page_token'] is not 'null'):
-    Apartments = open("/home/pi/OurMemphis/apartmentdata/Apartments_in_memphis" + str(count) + ".json","w")
-    time.sleep(15)
-    all_json4 = next_page(MyKey,str(all_json['next_page_token']),Apartments)
-    count += 1
+count = 0
+iteration = 0
+MyType = ['apartments in north memphis','apartments in south memphis','apartments in east memphis','apartments in southeast memphis','apartments in northeast memphis']
+naming = ['north','south','east','southeast','northeast']
+for search in MyType:
+    Apartments = open("/home/pi/OurMemphis/apartmentdata/Apartments_in_" + naming[iteration] + "memphis.json","w")
+    all_json = GoogPlac(100,search,MyKey,Apartments)
+    while(1):
+        if('next_page_token' in all_json):
+            Apartments = open("/home/pi/OurMemphis/apartmentdata/Apartments_in_" + naming[iteration] + "memphis" + str(count) + ".json","w")
+            time.sleep(15)
+            print "NEXT"
+            all_json = next_page(MyKey,str(all_json['next_page_token']),Apartments)
+            count += 1
+        else:
+            break
+    iteration += 1
