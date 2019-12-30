e795ead5d7baa71045a5750bd44523f4c1c3ef8c
Miclain Keffeler <mkkeffeler@crimson.ua.edu>
2019-12-29 19:49:44 -0600

    all the stuff

--- crime_data_parse.py
+++ crime_data_parse.py
@@ -68,10 +68,14 @@ while(iterate < len(input_files)):
             all_dates.append(line[9])
         iterate += 1
 iterate = 0
-
 crime_summary = {}
-for lat in all_lats:
-    crime_summary[lat] = {'name':all_names[count],'address':all_addresses[count],'lats': lat ,'longs': all_longs[count],'type': all_types[count],'date':all_dates[count]}
+#print "Total Lat Count: " + str((len(set(merged))))
+print "Total Long Count: " + str((len(set(all_longs))))
+print "Total Address Count: " + str((len(set(all_addresses))))
+print "Total Types Count: " + str((len(set(all_types))))
+print "Total Date Count: " + str((len(set(all_dates))))
+for lat in all_longs:
+    crime_summary[lat] = {'name':all_names[count],'address':all_addresses[count],'longs': lat ,'lats': all_lats[count],'type': all_types[count],'date':all_dates[count]}
     count += 1
 print "Total Crime Count: " + str((len(crime_summary.keys())))
 outfile.write(json.dumps(crime_summary,indent=4,sort_keys=True))
